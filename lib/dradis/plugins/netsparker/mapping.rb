c01261c6a327320a42e483428a13dda855b8ad16
Matt Budz <matt.budz@protonmail.com>
2024-03-07 17:37:40 +0100

    add default mappings

--- lib/dradis/plugins/netsparker/mapping.rb
+++ lib/dradis/plugins/netsparker/mapping.rb
@@ -1,6 +1,23 @@
 module Dradis::Plugins::Netsparker
   module Mapping
     def self.default_mapping
+      {
+        'evidence' => {
+          'URL' => '{{ netsparker[evidence.url] }}',
+          'Request' => 'bc.. {{ netsparker[evidence.rawrequest] }}',
+          'Response' => 'bc.. {{ netsparker[evidence.rawresponse] }}',
+          'VulnerableParameter' => 'bc. {{ netsparker[evidence.vulnerableparameter] }}',
+          'VulnerableParameterType' => 'bc. {{ netsparker[evidence.vulnerableparametertype] }}',
+          'VulnerableParameterValue' => 'bc. {{ netsparker[evidence.vulnerableparametervalue] }}'
+        },
+        'issue' => {
+          'Title' => '{{ netsparker[issue.title] }}',
+          'Severity' => '{{ netsparker[issue.severity] }}',
+          'Certainty' => '{{ netsparker[issue.certainty] }}',
+          'Description' => '{{ netsparker[issue.description] }}',
+          'Remedy' => '{{ netsparker[issue.remedy] }}'
+        }
+      }
     end
   end
 end

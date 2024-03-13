476857a0f4dd81b2efef15980bb1f112688ec494
Aaron Manaloto <mail@aapomm.io>
2024-03-13 17:05:16 +0800

    Create nodes for different vulnerability URLs

--- lib/dradis/plugins/netsparker/importer.rb
+++ lib/dradis/plugins/netsparker/importer.rb
@@ -7,16 +7,16 @@ module Dradis::Plugins::Netsparker
     # The framework will call this function if the user selects this plugin from
     # the dropdown list and uploads a file.
     # @returns true if the operation was successful, false otherwise
-    def import(params={})
-      file_content    = File.read( params.fetch(:file) )
+    def import(params = {})
+      file_content    = File.read(params.fetch(:file))
 
-      logger.info{'Parsing Netsparker output file...'}
-      @doc = Nokogiri::XML( file_content )
-      logger.info{'Done.'}
+      logger.info { 'Parsing Netsparker output file...' }
+      @doc = Nokogiri::XML(file_content)
+      logger.info { 'Done.' }
 
       if @doc.xpath('/netsparker').empty?
-        error = "No scan results were detected in the uploaded file (/netsparker). Ensure you uploaded an Netsparker XML report."
-        logger.fatal{ error }
+        error = 'No scan results were detected in the uploaded file (/netsparker). Ensure you uploaded an Netsparker XML report.'
+        logger.fatal { error }
         content_service.create_note text: error
         return false
       end
@@ -34,34 +34,39 @@ module Dradis::Plugins::Netsparker
       # Create Nodes from the <url> tags
       host_node_label = xml_host.at_xpath('./url').text
       host_node_label = URI.parse(host_node_label).host rescue host_node_label
-      logger.info{ "\t\t => Creating new host: #{host_node_label}" }
+      logger.info { "\t\t => Creating new host: #{host_node_label}" }
       host_node = content_service.create_node(label: host_node_label, type: :host)
 
       @doc.xpath('/netsparker/vulnerability').each do |xml_vuln|
         process_vuln(xml_vuln, host_node)
       end
-      
     end
 
     def process_vuln(xml_vuln, host_node)
-      type = xml_vuln.at_xpath('./type').text()
+      type = xml_vuln.at_xpath('./type').text
 
       # Create Issues using the Issue template
-      logger.info{ "\t\t => Creating new Issue: #{type}" }
+      logger.info { "\t\t => Creating new Issue: #{type}" }
 
       issue_text = template_service.process_template(template: 'issue', data: xml_vuln)
       issue = content_service.create_issue(text: issue_text, id: type)
 
       # Create Evidence using the Evidence template
       # Associate the Evidence with the Node and Issue
-      logger.info{ "\t\t => Creating new evidence" }
+      logger.info { "\t\t => Creating new evidence" }
       evidence_content = template_service.process_template(
         template: 'evidence', data: xml_vuln
       )
+
+      url = xml_vuln.at_xpath('./url').text
+      node =
+        # If the URL is a valid URI and is not the same as the host_node, create a new node
+        if url =~ URI::DEFAULT_PARSER.regexp[:ABS_URI] && URI(url).host != host_node.label
+          content_service.create_node(label: URI(url).host, type: :host)
+        end
       content_service.create_evidence(
-        issue: issue, node: host_node, content: evidence_content
+        issue: issue, node: node || host_node, content: evidence_content
       )
     end
-
   end
 end
--- spec/dradis-netsparker_spec.rb
+++ spec/dradis-netsparker_spec.rb
@@ -38,48 +38,53 @@ module Dradis::Plugins
       end
     end
 
-    it "creates the expected Node, Issue, and Evidence from the example file" do
+    it 'creates the expected Node, Issue, and Evidence from the example file' do
       expect(@content_service).to receive(:create_node).with(hash_including label: 'localhost', type: :host)
 
       expect(@content_service).to receive(:create_issue) do |args|
-        expect(args[:text]).to include("#[Title]#\nPassword over http")
-        expect(args[:id]).to eq("PasswordOverHttp")
+        expect(args[:text]).to include('All sensitive data should be transferred over HTTPS rather than HTTP')
+        expect(args[:id]).to eq('PasswordOverHttp')
         OpenStruct.new(args)
       end
 
       expect(@content_service).to receive(:create_evidence) do |args|
         expect(args[:content]).to include("#[Request]#\nbc.. GET /login HTTP/1.1")
-        expect(args[:issue].id).to eq("PasswordOverHttp")
-        expect(args[:node].label).to eq("localhost")
+        expect(args[:issue].id).to eq('PasswordOverHttp')
+        expect(args[:node].label).to eq('localhost')
       end
 
       @importer.import(file: 'spec/fixtures/files/example.xml')
     end
 
-    it "creates than one instance of Evidence for a single Issue" do
+    it 'creates than one instance of Evidence for a single Issue' do
       expect(@content_service).to receive(:create_node).with(hash_including label: 'snorby.org', type: :host)
 
       expect(@content_service).to receive(:create_issue) do |args|
-        expect(args[:text]).to include("#[Title]#\nEmail disclosure")
-        expect(args[:id]).to eq("EmailDisclosure")
+        expect(args[:text]).to include("#[Severity]#\nInformation")
+        expect(args[:id]).to eq('EmailDisclosure')
         OpenStruct.new(args)
       end
 
       expect(@content_service).to receive(:create_evidence) do |args|
         expect(args[:content]).to include("#[URL]#\nhttps://snorby.org/foo")
-        expect(args[:issue].id).to eq("EmailDisclosure")
-        expect(args[:node].label).to eq("snorby.org")
+        expect(args[:issue].id).to eq('EmailDisclosure')
+        expect(args[:node].label).to eq('snorby.org')
       end.once
 
       expect(@content_service).to receive(:create_evidence) do |args|
         expect(args[:content]).to include("#[URL]#\nhttps://snorby.org/bar")
-        expect(args[:issue].id).to eq("EmailDisclosure")
-        expect(args[:node].label).to eq("snorby.org")
+        expect(args[:issue].id).to eq('EmailDisclosure')
+        expect(args[:node].label).to eq('snorby.org')
       end.once
 
       @importer.import(file: 'spec/fixtures/files/example-evidence.xml')
     end
 
-  end
+    it 'creates multiple nodes for additional websites' do
+      expect(@content_service).to receive(:create_node).with(hash_including label: 'snorby.org', type: :host)
+      expect(@content_service).to receive(:create_node).with(hash_including label: 'example.com', type: :host)
 
+      @importer.import(file: 'spec/fixtures/files/multiple-nodes.xml')
+    end
+  end
 end

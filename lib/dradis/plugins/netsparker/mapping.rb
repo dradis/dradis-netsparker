91efb1acf49fd9c08cd5215285e97fcdff21f5a2
Matt Budz <matt.budz@protonmail.com>
2024-03-18 19:24:09 +0100

    use constant for default mappings

--- lib/dradis/plugins/netsparker/mapping.rb
+++ lib/dradis/plugins/netsparker/mapping.rb
@@ -1,23 +1,21 @@
 module Dradis::Plugins::Netsparker
   module Mapping
-    def self.default_mapping
-      {
-        'evidence' => {
-          'URL' => '{{ netsparker[evidence.url] }}',
-          'Request' => 'bc.. {{ netsparker[evidence.rawrequest] }}',
-          'Response' => 'bc.. {{ netsparker[evidence.rawresponse] }}',
-          'VulnerableParameter' => 'bc. {{ netsparker[evidence.vulnerableparameter] }}',
-          'VulnerableParameterType' => 'bc. {{ netsparker[evidence.vulnerableparametertype] }}',
-          'VulnerableParameterValue' => 'bc. {{ netsparker[evidence.vulnerableparametervalue] }}'
-        },
-        'issue' => {
-          'Title' => '{{ netsparker[issue.title] }}',
-          'Severity' => '{{ netsparker[issue.severity] }}',
-          'Certainty' => '{{ netsparker[issue.certainty] }}',
-          'Description' => '{{ netsparker[issue.description] }}',
-          'Remedy' => '{{ netsparker[issue.remedy] }}'
-        }
+    DEFAULT_MAPPING = {
+      evidence: {
+        'URL' => '{{ netsparker[evidence.url] }}',
+        'Request' => 'bc.. {{ netsparker[evidence.rawrequest] }}',
+        'Response' => 'bc.. {{ netsparker[evidence.rawresponse] }}',
+        'VulnerableParameter' => 'bc. {{ netsparker[evidence.vulnerableparameter] }}',
+        'VulnerableParameterType' => 'bc. {{ netsparker[evidence.vulnerableparametertype] }}',
+        'VulnerableParameterValue' => 'bc. {{ netsparker[evidence.vulnerableparametervalue] }}'
+      },
+      issue: {
+        'Title' => '{{ netsparker[issue.title] }}',
+        'Severity' => '{{ netsparker[issue.severity] }}',
+        'Certainty' => '{{ netsparker[issue.certainty] }}',
+        'Description' => '{{ netsparker[issue.description] }}',
+        'Remedy' => '{{ netsparker[issue.remedy] }}'
       }
-    end
+    }.freeze
   end
 end

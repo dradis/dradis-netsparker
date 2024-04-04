59f8d1f35f6d8b23b49503ca07e3383c9ab6fb6c
Matt Budz <matt.budz@protonmail.com>
2024-04-04 18:04:20 +0200

    move fields files to constant

--- lib/dradis/plugins/netsparker/mapping.rb
+++ lib/dradis/plugins/netsparker/mapping.rb
@@ -17,5 +17,52 @@ module Dradis::Plugins::Netsparker
         'Remedy' => '{{ netsparker[issue.remedy] }}'
       }
     }.freeze
+
+    SOURCE_FIELDS = {
+      evidence: [
+        'evidence.rawrequest',
+        'evidence.rawresponse',
+        'evidence.url',
+        'evidence.vulnerableparameter',
+        'evidence.vulnerableparametertype',
+        'evidence.vulnerableparametervalue'
+      ],
+      issue: [
+        'issue.actions_to_take',
+        'issue.certainty',
+        'issue.classification_asvs40',
+        'issue.classification_capec',
+        'issue.classification_cvss_vector',
+        'issue.classification_cvss_base_value',
+        'issue.classification_cvss_base_severity',
+        'issue.classification_cvss_environmental_value',
+        'issue.classification_cvss_environmental_severity',
+        'issue.classification_cvss_temporal_value',
+        'issue.classification_cvss_temporal_severity',
+        'issue.classification_cwe',
+        'issue.classification_disastig',
+        'issue.classification_hipaa',
+        'issue.classification_iso27001',
+        'issue.classification_nistsp80053',
+        'issue.classification_owasp2013',
+        'issue.classification_owasp2017',
+        'issue.classification_owasp2021',
+        'issue.classification_owasppc',
+        'issue.classification_pci31',
+        'issue.classification_pci32',
+        'issue.classification_wasc',
+        'issue.description',
+        'issue.external_references',
+        'issue.extrainformation',
+        'issue.impact',
+        'issue.knownvulnerabilities',
+        'issue.remedy',
+        'issue.remedy_references',
+        'issue.required_skills_for_exploitation',
+        'issue.severity',
+        'issue.title',
+        'issue.type'
+      ]
+    }.freeze
   end
 end
